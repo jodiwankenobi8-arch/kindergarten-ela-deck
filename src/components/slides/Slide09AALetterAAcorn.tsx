007e550865f0ea4f902f40a074a4d9cf8d29b4d8
jodiwankenobi8-arch <jodiwankenobi8@gmail.com>
2026-02-24 19:54:56 -0500

    Add files via upload

---
+++ src/components/slides/Slide09AALetterAAcorn.tsx
@@ -0,0 +1,18 @@
+import { UFLISlideLayout } from '../UFLISlideLayout';
+import { UFLISlideProps } from './UFLISlideProps';
+import letterAAcornImg from 'figma:asset/ee78aed28fc93977e29c53f9c0b16b48a4699a9a.png';
+
+export function Slide09AALetterAAcorn({ timerProps }: UFLISlideProps = {}) {
+  return (
+    <UFLISlideLayout timerProps={timerProps}>
+      <div className="flex items-center justify-center h-full w-full">
+        <img 
+          src={letterAAcornImg} 
+          alt="Letter A with Acorn"
+          className="object-contain"
+          style={{ width: '95%', height: '95%' }}
+        />
+      </div>
+    </UFLISlideLayout>
+  );
+}
